f934ada195431d5f65b06383927cbeef36effb08
Dmitry Tolochko <DimaTol36@gmail.com>
2022-10-24 15:49:38 +0500

    Second commit
    Добавил папки, сделал префаб оленя и удаление его как игрового объекта, когда он умирает

--- Assets/Scripts/Deer.cs
+++ Assets/Scripts/Deer.cs
@@ -18,30 +18,49 @@ enum Age
 
 public class Deer : MonoBehaviour
 {
+    // public Deer()
+    // {
+    //     transform.position = new Vector3(random.Next(-1, 2), random.Next(-1, 2), 0);
+    //     StartCoroutine(GetOlder());
+    //     rb = GetComponent<Rigidbody2D>();
+    // }
+
     private const int speed = 2;
     private System.Random random = new System.Random();
     public const int a = 7;
     public const int b = 4;
     public Rigidbody2D rb;
 
-    private float x;
-    private float y;
+    private float x = 0;
+    private float y = 0;
 
     private Age currentAge;
+    public bool IsIll = false;
 
     private DateTime time = new DateTime();
 
     void Start()
     {
+        transform.position = new Vector3(random.Next(-1, 2), random.Next(-1, 2), 0);
         StartCoroutine(GetOlder());
-        x = random.Next(-1, 2);
-        y = random.Next(-1, 2);
+        while (x == 0 & y == 0)
+        {
+            x = random.Next(-1, 2);
+            y = random.Next(-1, 2);
+        }
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Move();
+        if (IsIll)
+        {
+            //change sprite
+            StartCoroutine(Infection());
+        }
+        if (currentAge == Age.Dead)
+            gameObject.tag = "Dead";
     }
 
     private IEnumerator GetOlder()
@@ -60,6 +79,16 @@ public class Deer : MonoBehaviour
         yield return new WaitForSecondsRealtime(10);
         currentAge = Age.Dead;
         print("Умер");
+    }
+
+    private IEnumerator Infection()
+    {
+        yield return new WaitForSecondsRealtime(10);
+        currentAge = Age.Dead;
+        print("Умер от заражения");
+        StopCoroutine(GetOlder());
+        StopCoroutine(Infection());
+        IsIll = false;
     }
 
     private bool IsInBounds(Vector3 position)
---
+++ Assets/Scripts/DeerHerd.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DeerHerd : MonoBehaviour
+{
+    public List<GameObject> deers = new List<GameObject>();
+    public GameObject deer;
+
+    void Start()
+    {
+        deers.Add(Instantiate(deer, new Vector3(0, 0, 0), Quaternion.identity));
+    }
+
+    void Update()
+    {
+        if (GameObject.FindWithTag("Dead") != null)
+            GameObject.Destroy(GameObject.FindWithTag("Dead"));
+    }
+    private void OnMouseDown() 
+    {
+        Instantiate(deer, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
